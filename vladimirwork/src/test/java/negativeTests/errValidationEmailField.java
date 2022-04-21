36dd513b8c0fb27919a56b73692e4ae5e40deac6
Nikita <ni.bunovskii@severmed.com>
2022-04-21 12:48:09 +0100

    Ok, this looks better :)

--- vladimirwork/src/test/java/negativeTests/errValidationEmailField.java
+++ vladimirwork/src/test/java/negativeTests/errValidationEmailField.java
@@ -1,14 +1,16 @@
 package negativeTests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.safari.SafariDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import preConditions.errMessages;
 import preConditions.variables;
 
-public class errMaximumEmailField {
+public class errValidationEmailField {
     static int i = 1;
     static SafariDriver driver;
     @BeforeTest
@@ -27,12 +29,17 @@ public class errMaximumEmailField {
         while ((!driver.getPageSource().contains(errMessages.errEmail))) {
             driver.findElement(By.id(variables.emailInput)).sendKeys("B");
             i += 1;
-            driver.findElement(By.id(variables.submitBtn)).click();
             if (i > 255 && !driver.getPageSource().contains(errMessages.errEmail)) {
                 System.out.println("Limit is more than 256, no validation error. Terminated.");
                 break;
             }
         }
+    }
+    @Test (dependsOnMethods = "errMaxEmailField")
+    public static void errMinEmailField() {
+        while (((driver.getPageSource().contains(errMessages.errEmail)))) {
+            driver.findElement(By.id(variables.emailInput)).sendKeys(Keys.BACK_SPACE);
+        } Assert.assertFalse(driver.getPageSource().contains(errMessages.errEmail));
     }
 }
 
